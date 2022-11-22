7caf053aa4d30e7ce2a0b0e0ea208800af22ee55
mrsparki <blagoyhristov03@gmail.com>
2022-11-22 18:53:56 +0200

    Add project files.

---
+++ Project1/Teste.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void task2() {
+    int rows, columns;
+    cout << "Enter rows: ";
+    cin >> rows;
+    cout << "Enter columns: ";
+    cin >> columns;
+    srand(time(NULL));
+    int** arr = new int* [rows];
+    for (int i = 0; i < rows; i++)
+    {
+        arr[i] = new int[columns];
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i][j] = rand() % 100 + 1;
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+int main() {
+	task2();
+	return 0;
+}
